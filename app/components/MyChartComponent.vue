fed0b020701ca21ce4363fb64b101b9e9be618ab
Dennis Adriaansen <dennis.a@socialdeal.nl>
2025-03-19 09:05:04 +0100

    Try to wrap VisContainer and its imports

---
+++ app/components/MyChartComponent.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { VisXYContainer, VisLine, VisAxis } from "@unovis/vue";
+
+type DataRecord = { x: number; y: number };
+
+const data: DataRecord[] = [
+  { x: 0, y: 0 },
+  { x: 1, y: 2 },
+  { x: 2, y: 1 },
+];
+</script>
+
+<template>
+  <div>
+    <VisXYContainer :height="600">
+      <VisLine :data="data" :x="(d) => d.x" :y="(d) => d.y" />
+      <VisAxis type="x" />
+      <VisAxis type="y" />
+    </VisXYContainer>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,24 +1,8 @@
-<script lang="ts" setup>
-import { VisXYContainer, VisLine, VisAxis } from "@unovis/vue";
-
-type DataRecord = { x: number; y: number };
-
-const data: DataRecord[] = [
-  { x: 0, y: 0 },
-  { x: 1, y: 2 },
-  { x: 2, y: 1 },
-];
-</script>
-
 <template>
   <div>
     <TestSlot>
       <ClientOnly>
-        <VisXYContainer :height="600">
-          <VisLine :data="data" :x="(d) => d.x" :y="(d) => d.y" />
-          <VisAxis type="x" />
-          <VisAxis type="y" />
-        </VisXYContainer>
+        <MyChartComponent></MyChartComponent>
       </ClientOnly>
     </TestSlot>
   </div>
